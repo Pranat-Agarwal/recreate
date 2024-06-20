e75c58aed3f0eb3c489dd3b7c2d6d31e27a5dc9f
Pranat <pranatagarwal100@gmail.com>
2024-06-21 00:40:43 +0530

    13 june

--- app/src/main/java/com/example/recreate/MainActivity.kt
+++ app/src/main/java/com/example/recreate/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.recreate
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,21 +17,4 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    fun phone(view: View){
-        var intent:Intent=Intent(Intent.ACTION_DIAL,Uri.parse("tel:7302708153"))
-        startActivity(intent)
-    }
-
-    fun web(view: View){
-        var intent:Intent=Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"))
-    }
-
-    fun cal(view: View){
-        var intent:Intent=Intent(Intent.ACTION_DATE_CHANGED)
-    }
-
-    fun alarm(view: View){
-        var intent:Intent=Intent(Intent.ACTION_QUICK_CLOCK)
-    }
-
 }
---
+++ app/src/main/java/com/example/recreate/java/calculator.java
@@ -0,0 +1,11 @@
+package com.example.recreate.java;
+
+public class calculator {
+    public int add (int a,int b){
+        return a+b;
+    }
+
+    public int mul(int a,int b){
+        return a*b;
+    }
+}
