61b53d461126ec74c2a63081cd0fc4b6421a5623
Pranat <pranatagarwal100@gmail.com>
2024-06-20 21:44:43 +0530

    assignment 4

--- app/src/main/java/com/example/recreate/MainActivity.kt
+++ app/src/main/java/com/example/recreate/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.recreate
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            next()
+        }
+    }
+
+    private fun next() {
+        val editText = findViewById<EditText>(R.id.editTextText)
+        val message = editText.text.toString()
+        val intent: Intent = Intent(this, loginpage::class.java).also {
+            it.putExtra("mm", message)
+            startActivity(it)
         }
     }
 }
