1fd9d543c3e9a5c94ebff200e12825f357392521
Pranat <pranatagarwal100@gmail.com>
2024-06-22 00:01:12 +0530

    18 june

--- app/src/main/java/com/example/recreate/MainActivity.kt
+++ app/src/main/java/com/example/recreate/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.example.recreate
 
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
-    fun show(view : View)
-    {
-        var et:EditText= findViewById(R.id.editTextText)
-        var ch=et.text.toString()
-        var st:TextView=findViewById(R.id.textView)
-        st.setText(ch)
+    fun getweather(cityname:String):String{
+        return "{ temp:32 , windspeed:40 }"
+    }
+
+    fun gettemp(city:String):Int{
+        return 25
     }
 }
