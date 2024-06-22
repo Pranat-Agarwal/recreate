d612b1f206b1498fc5ea28349b4d2672821b3870
Pranat <pranatagarwal100@gmail.com>
2024-06-22 22:22:39 +0530

    20 june

--- app/src/main/java/com/example/recreate/adpater.kt
+++ app/src/main/java/com/example/recreate/adpater.kt
@@ -4,15 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import coil.load
 
 class adpater(var listmmarsphotos: List<Any>) :RecyclerView.Adapter<adpater.marsviewholder> {
     class marsviewholder(itemView: View):RecyclerView.ViewHolder(itemView) {
-        var textview:TextView=itemView.findViewById(android.R.id.text1)
+        var textview:TextView=itemView.findViewById(R.id.textView2)
+        var marsimageview:ImageView=itemView.findViewById(R.id.imageView2)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): marsviewholder {
-        var rowplank=LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1,parent,false)
+        var rowplank=LayoutInflater.from(parent.context).inflate(R.layout.rowlayout,parent,false)
         return  marsviewholder(rowplank)
     }
 
@@ -21,7 +24,9 @@ class adpater(var listmmarsphotos: List<Any>) :RecyclerView.Adapter<adpater.mars
     }
 
     override fun onBindViewHolder(holder: marsviewholder, position: Int) {
-        holder.textview.setText((listmmarsphotos.get(position).imgsrc))
+        var url:String=listmmarsphotos.get(position).imgsrc
+        holder.textview.setText(url)
+        holder.marsimageview.load(url)
     }
 
 }
