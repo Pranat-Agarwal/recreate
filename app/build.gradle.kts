527740f5a253277ce81da56cf35190b5bbd426fc
Pranat <pranatagarwal100@gmail.com>
2024-07-07 19:02:43 +0530

    assesment db on recycler view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation ("androidx.room:room-runtime:2.3.0")
+    implementation("androidx.room:room-compiler:2.3.0")
+    implementation ("androidx.room:room-ktx:2.3.0")
+
 }
---
+++ app/src/main/java/com/example/recreate/kotlin/UserDao.kt
@@ -0,0 +1,14 @@
+package com.example.recreate.kotlin
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun insert(user: User)
+
+    @Query("SELECT * FROM user_table")
+    suspend fun getAllUsers(): List<User>
+}
